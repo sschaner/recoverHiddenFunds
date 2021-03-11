54457edbc8edf6316bd7f01ebdc88eca51fb087d
Steve Schaner <schanerst@gmail.com>
2021-03-11 18:12:08 -0500

    deleted hamburger testing files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from './global';
-import { theme } from './theme';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
@@ -13,20 +10,18 @@ import ContactScreen from './screens/ContactScreen';
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <GlobalStyles />
-        <Header />
-        <main>
-          <Route path='/' component={HomeScreen} exact />
-          <Route path='/company' component={CompanyScreen} />
-          <Route path='/process' component={ProcessScreen} />
-          <Route path='/faq' component={FAQScreen} />
-          <Route path='/contact' component={ContactScreen} />
-        </main>
-        <Footer />
-      </Router>
-    </ThemeProvider>
+    <Router>
+      <Header />
+      <main>
+        <Route path='/' component={HomeScreen} exact />
+        <Route path='/company' component={CompanyScreen} />
+        <Route path='/process' component={ProcessScreen} />
+        <Route path='/faq' component={FAQScreen} />
+        <Route path='/contact' component={ContactScreen} />
+      </main>
+
+      <Footer />
+    </Router>
   );
 };
 
--- frontend/src/theme.js
+++
@@ -1,5 +0,0 @@
-export const theme = {
-  primaryColor: '#526044',
-  secondaryColor: '#cea176',
-  mobile: '576px',
-};
